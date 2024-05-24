063ff1decdc3ca816aaf3e212e2b88305fd49345
Brayan Hernandez <hernandezbrayan697@gmail.com>
2024-05-24 16:00:46 -0500

    Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventariov2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
